eb75827307c8c006ecbf960f8f53ef4663915682
Michael Bradley <michael.bradley@hotmail.co.uk>
2019-06-11 19:47:19 +0100

    Install PyQode for a code editor

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,9 +2,12 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPalette
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMessageBox, QMainWindow
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMessageBox, QMainWindow, QTextEdit
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-
+from pyqode.core import api
+from pyqode.core import modes
+from pyqode.core import panels
+from pyqode.qt import QtWidgets
 
 def on_top_clicked():
     alert = QMessageBox()
@@ -26,14 +29,21 @@ if __name__ == '__main__':
     app.setPalette(palette)
     app.setStyleSheet("QPushButton { margin: 1ex; padding: 1ex }")
 
-    window = QWidget()
-    layout = QVBoxLayout()
-    button = QPushButton('Top')
-    button.clicked.connect(on_top_clicked)
-    layout.addWidget(button)
-    layout.addWidget(QPushButton('Bottom'))
-    window.setLayout(layout)
-    main_window.setCentralWidget(window)
+    editor = api.CodeEdit()
+    # start the backend as soon as possible
+    editor.backend.start('code-editor-server.py')
+
+    # append some modes and panels
+    editor.modes.append(modes.CodeCompletionMode())
+    editor.modes.append(modes.PygmentsSyntaxHighlighter(editor.document()))
+    editor.modes.append(modes.CaretLineHighlighterMode())
+    editor.panels.append(panels.SearchAndReplacePanel(),
+                         api.Panel.Position.BOTTOM)
+
+    # open a file
+    editor.file.open("example.txt")
+
+    main_window.setCentralWidget(editor)
     main_window.show()
 
     exit_code = app.exec()      # 2. Invoke appctxt.app.exec_()
